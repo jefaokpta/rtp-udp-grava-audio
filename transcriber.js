eedd1a01ae9951b1ead1bf451ffb563eb29da577
jefaokpta <jefaokpta@hotmail.com>
2024-11-18 12:37:30 -0300

    tentar asyncrono

--- transcriber.js
+++ transcriber.js
@@ -11,32 +11,37 @@ const HOST = '0.0.0.0:9999';
 const SWAP16 = false;
 const OUTPUT_FILE = 'audios/audio.raw';
 
-// Create an instance of RtpUdpServerSocket
-const rtpServer = new RtpUdpServerSocket(HOST, SWAP16, OUTPUT_FILE);
+
+async function start() {
+    // Create an instance of RtpUdpServerSocket
+    const rtpServer = new RtpUdpServerSocket(HOST, SWAP16, OUTPUT_FILE);
 
 // Configuration for Google Speech API
-const config = {
-    encoding: 'LINEAR16',
-    sampleRateHertz: 16000,
-    languageCode: 'pt-BR',
-};
+    const config = {
+        encoding: 'LINEAR16',
+        sampleRateHertz: 16000,
+        languageCode: 'pt-BR',
+    };
 
 // Callback function to handle transcription results
-const transcriptCallback = (transcript, isFinal) => {
-    if (isFinal) {
-        console.log(`Final transcription: ${transcript}`);
-    } else {
-        console.log(`Partial transcription: ${transcript}`);
-    }
-};
+    const transcriptCallback = (transcript, isFinal) => {
+        if (isFinal) {
+            console.log(`Final transcription: ${transcript}`);
+        } else {
+            console.log(`Partial transcription: ${transcript}`);
+        }
+    };
 
 // Callback function to handle full results
-const resultsCallback = (results) => {
-    console.log('Full results:', results);
-};
+    const resultsCallback = (results) => {
+        console.log('Full results:', results);
+    };
 
 // Create an instance of GoogleSpeechProvider
-const googleSpeechProvider = new GoogleSpeechProvider(config, rtpServer, transcriptCallback, resultsCallback);
+    const googleSpeechProvider = new GoogleSpeechProvider(config, rtpServer, transcriptCallback, resultsCallback);
+}
+
+start();
 
 // mantendo o servidor ativo
 process.stdin.resume();
